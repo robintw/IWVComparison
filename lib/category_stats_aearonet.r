39b3d77527a6711572d58cb7a48d6022355c60c1
Robin Wilson <robin@rtwilson.com>
2014-02-14 18:47:59 +0000

    Changed category stats to be functions and put in lib/

---
+++ lib/category_stats_aearonet.r
@@ -0,0 +1,17 @@
+run_category_stats_aeronet <- function()
+{
+  # Split mlist.aeronet into post 2007
+  mlist.aeronet.post2007 <- lapply(mlist.aeronet, get_post_2007)
+  
+  r.post2007 <- calculate_category_stats_aeronet(mlist.aeronet.post2007)
+  r.all <- calculate_category_stats_aeronet(mlist.aeronet)
+  
+  dateranges <- list(All = 1, Post2007 = 2)
+  r.all$daterange <- 1
+  r.post2007$daterange <- 2
+  
+  r <- rbind(r.all, r.post2007)
+  r$daterange <- factor(r$daterange, labels=c('All', 'Post2007'))
+  
+  return(r)
+}
---
+++ lib/category_stats_ar.r
@@ -0,0 +1,17 @@
+# Get stats by category for the AERONET-Radiosonde validation
+run_category_stats_ar <- function()
+{
+  mlist.ar.post2007 <- lapply(mlist.ar, get_post_2007)
+  
+  r.post2007 <- calculate_category_stats_ar(mlist.ar.post2007)
+  r.all <- calculate_category_stats_ar(mlist.ar)
+  
+  dateranges <- list(All = 1, Post2007 = 2)
+  r.all$daterange <- 1
+  r.post2007$daterange <- 2
+  
+  r <- rbind(r.all, r.post2007)
+  r$daterange <- factor(r$daterange, labels=c('All', 'Post2007'))
+  
+  return(r)
+}
