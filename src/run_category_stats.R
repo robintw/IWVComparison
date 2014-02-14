e15f2622646293f4bb397b0bb73273a497561471
Robin Wilson <robin@rtwilson.com>
2014-02-14 18:57:13 +0000

    Added code to run category stats

---
+++ src/run_category_stats.R
@@ -0,0 +1,7 @@
+# Run all of the category statistics and save to CSV
+
+cat.stats.radiosonde <- run_category_stats_radiosonde()
+cat.stats.aeronet <- run_category_stats_aeronet()
+cat.stats.ar <- run_category_stats_ar()
+
+# TODO: Add CSV writing code here!
