70051f57bb3113379bbfc14948a6e85ef534ddb0
Robin Wilson <robin@rtwilson.com>
2017-01-16 18:32:47 +0000

    Removed out-of-date TODO message

--- src/run_category_stats.R
+++ src/run_category_stats.R
@@ -4,7 +4,6 @@ cat.stats.radiosonde <- run_category_stats_radiosonde()
 cat.stats.aeronet <- run_category_stats_aeronet()
 cat.stats.ar <- run_category_stats_ar()
 
-# TODO: Add CSV writing code here!
 write.csv(cat.stats.radiosonde, "results/Category_Stats_Radiosonde.csv")
 write.csv(cat.stats.aeronet, "results/Category_Stats_AERONET.csv")
 write.csv(cat.stats.ar, "results/Category_Stats_AERONET-Radiosonde.csv")
