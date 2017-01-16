4798df1d35e31ea0a33ace5b970107fd2cb8169c
Robin Wilson <robin@rtwilson.com>
2017-01-16 18:32:54 +0000

    Added comments

--- go.r
+++ go.r
@@ -1,3 +1,14 @@
+# Quickstart script for the code to run the IWV comparison used in
+# A global comparison of integrated water vapour estimates from WMO
+# radiosondes, AERONET sun photometers and GPS for the 17 year period
+# from 1997 to 2013
+# by Wilson, Hansen, Bingley and Milton
+#
+# See README.md for more information
+#
+#
+#
+#
 # Sets up system for ProjectTemplate and loads project
 
 # Load libraries, installing them if not available
